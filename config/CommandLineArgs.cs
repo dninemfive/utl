898feb4f8c219a95fd65e8616da91831bfe808f0
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-04-19 13:32:54 -0700

    more xml comments

---
+++ config/CommandLineArgs.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.utl
+{
+    /// <summary>
+    /// Handles automatically loading command-line arguments into variables.
+    /// </summary>
+    /// <example>
+    /// public static readonly string ExampleArg = CommandLineArgs.Get("example", CommandLineArgs.Parsers.FirstNonNullString);
+    /// </example>
+    public static class CommandLineArgs
+    {
+        private readonly static IntermediateArgs _intermediateArgs;
+        /// <summary>
+        /// Defines a parser which operates on the values recorded for a given variable by an <see cref="IntermediateArgs"/> instance and returns
+        /// an object of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to return.</typeparam>
+        /// <param name="values">A potentially <see langword="null"/> <see cref="IEnumerable{T}">IEnumerable</see>&lt;<see langword="string"/>&gt;
+        /// corresponding to the values passed after invoking a given variable's name.</param>
+        /// <param name="flag">If the <see cref="IntermediateArgs.Flags">flag</see> specified for the variable in question is present, 
+        /// <see langword="true"/>; otherwise, <see langword="false"/>.</param>
+        /// <returns>An object of type <typeparamref name="T"/>, if parsing was successful, or <see langword="null"/> if parsing was not successful.</returns>
+        public delegate T? Parser<T>(IEnumerable<string>? values, bool flag);
+        /// <summary>
+        /// Predefined <see cref="Parser{T}">parsers</see> for command-line args.
+        /// </summary>
+        public static class Parsers
+        {
+            /// <summary>
+            /// Selects the first <see langword="string"/> among the values which is not <see langword="null"/> and whose length is greater than 0.
+            /// </summary>
+            /// <remarks>Ignores the flag variable.</remarks>
+            public static Parser<string> FirstNonNullOrEmptyString => (values, _) => values?.SkipWhile(x => string.IsNullOrEmpty(x)).First();
+            /// <summary>
+            /// Returns <inheritdoc cref="GetFlag(string, char?)" path="/returns"/>
+            /// </summary>
+            public static Parser<bool> Flag => (enumerable, flag) => enumerable is not null || flag;
+            /// <summary>
+            /// Returns the potentially <see langword="null"/> <see cref="IEnumerable{T}">IEnumerable</see>&lt;<see langword="string"/>&gt; corresponding
+            /// to the exact values passed when specifying the given variable.
+            /// </summary>
+            public static Parser<IEnumerable<string>?> Raw => (values, _) => values;
+        }
+        static CommandLineArgs()
+        {
+            _intermediateArgs = new(Environment.GetCommandLineArgs()[1..]);
+            foreach((int pos, string warning) in _intermediateArgs.Warnings)
+            {
+                Utils.DebugLog($"Error in command-line args at position {pos}: {warning}");
+            }
+        }
+        /// <summary>
+        /// Attempts to get the argument named <c><paramref name="argName"/></c> as type <typeparamref name="T"/> using the specified <c><paramref name="parser"/></c>,
+        /// returning <see langword="null"/> if unsuccessful.
+        /// </summary>
+        /// <typeparam name="T">The type of the variable to get.</typeparam>
+        /// <param name="argName">The command-line name of the variable to get.</param>
+        /// <param name="parser">The <see cref="Parser{T}"/> used to get the variable's value.</param>
+        /// <returns>An object of type <typeparamref name="T"/> if parsing was successful, or <see langword="null"/> otherwise.</returns>
+        public static T? TryGet<T>(string argName, Parser<T> parser)
+            => parser(_intermediateArgs[argName], false);
+        /// <summary>
+        /// Gets the specified <see cref="IntermediateArgs.Flags">command-line flag</see>.
+        /// </summary>
+        /// <param name="argName">The name of the flag to get.</param>
+        /// <param name="flag">The single-character abbreviation for the flag. If not specified, defaults to the lowercase equivalent of the first character
+        /// of the specified <c><paramref name="argName"/></c>.</param>
+        /// <returns><see langword="true"/> if the variable was defined at least once or its corresponding <see cref="IntermediateArgs.Flags">flag</see>
+        /// is present in the arguments, or <see langword="false"/> otherwise.</returns>
+        public static bool GetFlag(string argName, char? flag = null)
+            => Parsers.Flag(_intermediateArgs[argName], _intermediateArgs[flag ?? argName.First().ToLower()]);
+        /// <summary>
+        /// Gets the value of the argument named <c><paramref name="argName"/></c>, if present, and <b>throws an exception</b> if the argument is not found.
+        /// </summary>
+        /// <typeparam name="T"><inheritdoc cref="TryGet{T}(string, Parser{T})" path="/typeparam[@name='T']"/></typeparam>
+        /// <param name="argName"><inheritdoc cref="TryGet{T}(string, Parser{T})" path="/param[@name='argName']"/></param>
+        /// <param name="parser"><inheritdoc cref="TryGet{T}(string, Parser{T})" path="/param[@name='parser']"/></param>
+        /// <param name="exception">The exception to throw if the variable is not found. If not specified, a generic <see cref="Exception"/> is thrown.</param>
+        /// <returns>The value of the argument named <c><paramref name="argName"/></c>, if it exists.</returns>
+        public static T Get<T>(string argName, Parser<T> parser, Exception? exception = null)
+            => TryGet(argName, parser) ?? throw exception ?? new Exception($"Tried to get command-line argument {argName}, but it was not found!");
+    }
+}
---
+++ config/IntermediateArgs.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+#pragma warning disable IDE1006
+namespace d9.utl
+{
+    /// <summary>
+    /// Parses console _args formatted as an approximation of Unix console _args into a structure representing arguments and their values.
+    /// <br/><br/>
+    /// </summary>
+    /// <remarks>
+    /// For example,
+    /// <br/><c>program.exe -- asdf -f --arg1 69 --exampleList a b c d --arg2 42</c><br/>
+    /// is parsed to
+    /// <br/>
+    /// <code>
+    /// IntermediateArgs {
+    ///   _args: {
+    ///     // note that dictionaries are unordered; i sorted this for convenience
+    ///     arg1: ["69"]
+    ///     arg2: ["42"]
+    ///     exampleList: ["a", "b", "c", "d"]
+    ///   }
+    ///   flags: ['f']
+    ///   warnings: [
+    ///     "position 1: Encountered "--" but no currentKey to close.",
+    ///     "position 2: Encountered value "asdf" but no currentKey to add it to."
+    ///   ]
+    /// }
+    /// </code>
+    /// </remarks>
+    public partial record IntermediateArgs
+    {
+        /// <summary>
+        /// The arguments without dashes, organized by their keys.
+        /// </summary>
+        private Dictionary<string, ICollection<string>> _args { get; set; } = new();
+        /// <summary>
+        /// The flags set on the program. Note that flags can repeat, and that the <see cref="CommandLineArgParsers.Flag">default implementation</see>
+        /// counts occurrences of empty lists in <see cref="_args">_args</see> corresponding to its alias as occurrences of that key.
+        /// </summary>
+        private IEnumerable<char> Flags => _flags;
+        /// <inheritdoc cref="Flags"/>
+        private List<char> _flags { get; set; } = new();
+        /// <summary>
+        /// Any warnings generated due to syntax errors in the arguments and their respective positions.
+        /// </summary>
+        public IEnumerable<(int position, string message)> Warnings => _warnings;
+        /// <inheritdoc cref="Warnings"/>
+        private List<(int position, string message)> _warnings { get; set; } = new();
+        /// <summary>
+        /// Used to simplify parsing code in the constructor.
+        /// </summary>
+        private enum ArgCase { Terminator, Flag, Arg, Value }
+        /// <summary>
+        /// Constructs a new IntermediateArgs instance from the unparsed arguments passed to the program.
+        /// </summary>
+        /// <param name="args">The unparsed _args, corresponding to the <see langword="_args"/> keyword or the <c>_args</c> argument to a program's
+        /// <c>Main(<see langword="string"/>[] _args)</c> method.</param>
+        public IntermediateArgs(string[] args)
+        {
+            string? currentKey = null;
+            static ArgCase getCase(string arg)
+            {
+                if (arg == "--") return ArgCase.Terminator;
+                if (FlagMatcher().IsMatch(arg)) return ArgCase.Flag;
+                if (ArgMatcher().IsMatch(arg)) return ArgCase.Arg;
+                return ArgCase.Value;
+            };
+            int pos = 1;
+            foreach(string arg in args)
+            {
+                switch(getCase(arg))
+                {
+                    case ArgCase.Terminator:
+                        if (currentKey is not null) currentKey = null;
+                        else _warnings.Add((pos++, $"Encountered \"--\" but no currentKey to close."));
+                        break;
+                    case ArgCase.Flag:
+                        currentKey = null;
+                        _flags.Add(arg[1]);
+                        break;
+                    case ArgCase.Arg:
+                        currentKey = arg[2..];
+                        if (!_args.ContainsKey(currentKey)) _args[currentKey] = new List<string>();
+                        break;
+                    case ArgCase.Value:
+                        if (currentKey is not null) _args[currentKey].Add(arg);
+                        else _warnings.Add((pos, $"Encountered value \"{arg}\" but no currentKey to add it to."));
+                        break;
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the values corresponding to the specified key, if any.
+        /// </summary>
+        /// <param name="key">The key to look for in the underlying dictionary.</param>
+        /// <returns>If the key was present at least once in the _args, a non-null IEnumerable, with corresponding values, if there were any, in
+        /// order of appearance.<br/><br/>If the key was <em>not</em> present in the _args, <see langword="null"/>.</returns>
+        public IEnumerable<string>? this[string key] => _args.TryGetValue(key, out ICollection<string>? value) ? value : null;
+        public bool this[char flag] => Flags.Contains(flag);
+        /// <summary>
+        /// Tells whether the given key corresponds to the name of an argument which was passed to the program.
+        /// </summary>
+        /// <param name="key">The key to look for.</param>
+        /// <returns><see langword="true"/> if the key was found, or <see langword="false"/> otherwise.</returns>
+        public bool ContainsKey(string key) => _args.ContainsKey(key);
+        // gets error CS8795, but this is a bug: see 
+        [GeneratedRegex("-.")]
+        private static partial Regex FlagMatcher();
+        // see above comment
+        [GeneratedRegex("--.+")]
+        private static partial Regex ArgMatcher();
+    }
+}
