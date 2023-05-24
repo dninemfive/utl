6d7c9f04c361288b285dd54723a59cbf67c0bfb9
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-05-24 16:52:34 -0700

    maui control setup is confusing :gore:

--- utl-maui/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace utl_maui
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}
---
+++ utl-maui/Platforms/Windows/RichConsoleHandler.Windows.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Handlers;
+
+namespace utl_maui
+{
+    // All the code in this file is only included on Windows.
+    public partial class RichConsoleHandler : ViewHandler<RichConsoleHandler, RichConsole_Windows>
+    {
+        protected override RichConsole_Windows CreatePlatformView()
+        {
+            throw new NotImplementedException();
+        }
+        protected override void ConnectHandler(RichConsole_Windows platformView)
+        {
+            base.ConnectHandler(platformView);
+        }
+        protected override void DisconnectHandler(RichConsole_Windows platformView)
+        {
+            base.DisconnectHandler(platformView);
+        }
+    }
+    public class RichConsole_Windows { }
+}
