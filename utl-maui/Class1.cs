2f9684d191849fd470ca3e8a6eee2e9245cc15b4
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-05-24 16:33:41 -0700

    start work on MAUI console control

---
+++ utl-maui/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Controls;
+using d9.utl;
+
+namespace d9.utl.maui;
+public class RichConsole : View, IConsole
+{
+    public static readonly BindableProperty AutoScrollProperty
+        = BindableProperty.Create(nameof(AutoScroll), typeof(bool), typeof(RichConsole), true);
+    public bool AutoScroll
+    {
+        get => (bool)GetValue(AutoScrollProperty);
+        set => SetValue(AutoScrollProperty, value);
+    }
+    public static readonly BindableProperty BufferSizeProperty
+        = BindableProperty.Create(nameof(BufferSize), typeof(int), typeof(RichConsole), 8192);
+    public int BufferSize
+    {
+        get => (int)GetValue(BufferSizeProperty);
+        set => SetValue(BufferSizeProperty, value);
+    }
+    public void Write(object? obj) => throw new NotImplementedException();
+    public void WriteLine(object? obj) => throw new NotImplementedException();
+}
