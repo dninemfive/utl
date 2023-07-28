c8de3bf123fb839d8b6eb8b597ecf9a9f39930e3
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-07-28 08:50:00 -0700

    second pass at ParseQuery()

--- utils/NetUtils.cs
+++ utils/NetUtils.cs
@@ -7,24 +7,38 @@ using System.Threading.Tasks;
 namespace d9.utl;
 public static class NetUtils
 {
+    public class ParsedQuery
+    {
+        public IEnumerable<(string key, string value)> Items { get; private set; }
+        internal ParsedQuery(IEnumerable<(string key, string value)> items)
+        {
+            Items = items;
+        }
+        public IEnumerable<string> this[string key] => Items.Where(x => x.key == key).Select(x => x.value);
+        public IEnumerable<T> Get<T>(string key) where T : class
+            => this[key].Select(x => x as T).Where(x => x is not null)!;
+        public string? First(string key) => this[key].Any() ? this[key].First() : null;
+        public T? First<T>(string key) where T : class
+            => First(key) as T;
+    }
     /// <summary>
     /// Parses the query portion of a <see cref="Uri"/> and returns each item and its corresponding value.
     /// </summary>
     /// <remarks>Currently, if a query has duplicates of the same key, only the first one will be returned.</remarks>
     /// <returns>A dictionary</returns>
-    public static Dictionary<string, string> ParseQuery(this Uri uri)
+    public static ParsedQuery ParseQuery(this Uri uri)
     {
         string query = uri.Query[1..^0]; // slice the question mark away
-        Dictionary<string, string> result = new();
+        List<(string key, string value)> items = new();
         foreach(string item in query.Split("&"))
         {
             string[] split = item.Split("=");
-            if (result.ContainsKey(split[0])) continue;
-            result[split[0]] = split[1..^0].Aggregate((x, y) => $"{x}={y}");
+            if (split.Length < 2) continue;
+            items.Add((split[0], split[1..].Aggregate((x, y) => $"{x}{y}")));
         }
-        return result;
+        return new(items);
     }
-    public static Dictionary<string, string> ParseQuery(this string url) => new Uri(url).ParseQuery();
+    public static ParsedQuery ParseQuery(this string url) => new Uri(url).ParseQuery();
     public static string ToQuery(this IEnumerable<(string key, string value)> items)
         => $"?{items.Select(x => $"{x.key}={x.value}")
                     .Aggregate((x, y) => $"{x}&{y}")}";
