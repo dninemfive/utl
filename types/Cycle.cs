821f2ca30b0e54dd28d26fc70137940464234009
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-10-09 01:56:37 -0700

    add Cycles and GenericEnumerator

---
+++ types/Cycle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.utl;
+/// <summary>
+/// Represents the idea of a closed loop of values which can be iterated along.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Cycle<T> : IEnumerable<T>
+{
+    private readonly List<T> _list = new();
+    private int _index = 0;
+    private int Index
+    {
+        get => _index;
+        set => _index = offsetIndex(value);
+    }
+    private int offsetIndex(int i) => (_index + i) % _list.Count;
+    public Cycle(params T[] items) : this(items.ToList()) { }
+    public Cycle(IEnumerable<T> items)
+    {
+        _list = items.ToList();
+    }
+    public T CurrentItem => _list[Index];
+    public T NextItem() => _list[++Index];
+    public T PreviousItem() => _list[--Index];
+    /// <summary>
+    /// Inserts the specified <paramref name="item"/> before the <see cref="CurrentItem"/>.
+    /// </summary>
+    /// <param name="item">The item to insert.</param>
+    public void Prepend(T item)
+    {
+        _list.Insert(offsetIndex(-1), item);
+    }
+    /// <summary>
+    /// Gets the enumerator for this cycle, starting at the <see cref="CurrentItem">current item</see>
+    /// and continuing until it has looped back around to the previous element.
+    /// </summary>
+    /// <returns>Every item in the cycle exactly once, starting at the <see cref="CurrentItem">current item</see>.</returns>
+    public IEnumerator<T> GetEnumerator()
+    {
+        T[] array = new T[_list.Count];
+        for(int i = 0; i < _list.Count; i++)
+        {
+            array[i] = _list[offsetIndex(i)];
+        }
+        return new GenericEnumerator<T>(array);
+    }
+    /// <inheritdoc cref="GetEnumerator"/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        T[] array = new T[_list.Count];
+        for (int i = 0; i < _list.Count; i++)
+        {
+            array[i] = _list[offsetIndex(i)];
+        }
+        return new GenericEnumerator<T>(array);
+    }
+    /// <summary>
+    /// Gets the item at the specified <paramref name="offset"/> in the sequence.
+    /// </summary>
+    /// <param name="offset">The index of the item to select, relative to the current item.</param>
+    /// <returns>The item at the specified position.</returns>
+    public T this[int offset] => _list[offsetIndex(offset)];
+    public void Clear() => _list.Clear();
+}
---
+++ types/GenericEnumerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.utl;
+public class GenericEnumerator<T> : IEnumerator<T>, IDisposable
+{
+    private bool _disposedValue;
+    private int _index = -1;
+    private T[] _items;
+    public GenericEnumerator(params T[] items)
+    {
+        _items = items;
+    }
+    public T Current
+    {
+        get
+        {
+            try
+            {
+                return _items[_index];
+            } 
+            catch
+            {
+                throw new Exception($"GenericEnumerator<{typeof(T).Name}>.Current is undefined when _index = {_index}!");
+            }
+        }
+    }
+    object IEnumerator.Current => throw new NotImplementedException();
+
+    public bool MoveNext()
+    {
+        _index++;
+        return _index > 0 && _index < _items.Count;
+    }
+    public void Reset() => _index = -1;
+    /// <summary>
+    /// Not implemented, as there are no additional resources to dispose. See 
+    /// <see href="https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.ienumerator-1?view=net-7.0#notes-to-implementers">
+    /// here</see> for more information.
+    /// </summary>
+    #pragma warning disable CA1816 // "call GC.SuppressFinalize()": derived classes should implement Dispose() if needed
+    public void Dispose() { }
+    #pragma warning restore CA1816
+}
