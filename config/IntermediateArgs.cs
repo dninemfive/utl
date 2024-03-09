b64cdf3eba8bc07019c2bbaa823d5fd4c4c7f64a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-03-09 12:39:35 -0800

    remove debug print from IntermediateArgs

--- config/IntermediateArgs.cs
+++ config/IntermediateArgs.cs
@@ -65,7 +65,6 @@ public partial record IntermediateArgs
         int pos = 1;
         foreach(string arg in args)
         {
-            Console.WriteLine($"{arg} {getCase(arg)}");
             switch(getCase(arg))
             {
                 case ArgCase.Terminator:
