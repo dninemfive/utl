accd518c4b6c81f4cd41c85be2e71bac9f79918a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-17 12:34:32 -0600

    document and tweak the ILogComponents

--- logging/StreamLogComponent.cs
+++ logging/StreamLogComponent.cs
@@ -1,38 +1,60 @@
 ﻿namespace d9.utl;
+/// <summary>
+/// An <see cref="ILogComponent"/> which wraps a <see cref="Stream"/> and <see cref="StreamWriter"/>
+/// and allows them to be disposed.
+/// </summary>
 public class StreamLogComponent
     : ILogComponent, IDisposable, IAsyncDisposable
 {
     private Stream _stream;
-    private StreamWriter _sw;
+    private StreamWriter _streamWriter;
     private bool _disposed = false;
+    /// <summary>
+    /// Creates a <c>StreamLogComponent</c> from the specified <paramref name="stream"/>.
+    /// </summary>
+    /// <param name="stream">The <see cref="Stream"/> from which to create the component.</param>
     public StreamLogComponent(Stream stream)
     {
         _stream = stream;
-        _sw = new(_stream);
+        _streamWriter = new(_stream);
     }
-    public StreamLogComponent(StreamWriter sw)
+    /// <summary>
+    /// Creates a <c>StreamLogComponent</c> from the specified <paramref name="streamWriter"/>.
+    /// </summary>
+    /// <param name="streamWriter">The <see cref="StreamWriter"/> from which to create the component.</param>
+    public StreamLogComponent(StreamWriter streamWriter)
     {
-        _stream = sw.BaseStream;
-        _sw = sw;
+        _stream = streamWriter.BaseStream;
+        _streamWriter = streamWriter;
     }
+    /// <inheritdoc cref="ILogComponent.Write(object?)"/>
     public Task Write(object? obj)
-        => _sw.WriteAsync($"{obj}");
+        => _streamWriter.WriteAsync($"{obj}");
+    /// <inheritdoc cref="ILogComponent.WriteLine(object?)"/>
     public Task WriteLine(object? obj)
-        => _sw.WriteLineAsync($"{obj}");
+        => _streamWriter.WriteLineAsync($"{obj}");
+    /// <summary>
+    /// Implements <see cref="IDisposable"/>.
+    /// </summary>
     public void Dispose()
     {
         if(!_disposed)
         {
-            _sw.Dispose();
+            _streamWriter.Dispose();
             _stream.Dispose();
         }
+        GC.SuppressFinalize(this);
     }
+    /// <summary>
+    /// Implements <see cref="IAsyncDisposable"/>.
+    /// </summary>
     public async ValueTask DisposeAsync()
     {
         if(!_disposed)
         {
-            await _sw.DisposeAsync();
+            await _streamWriter.DisposeAsync();
             await _stream.DisposeAsync();
         }
+        GC.SuppressFinalize(this);
     }
 }
