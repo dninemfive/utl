2525aa9b1a8b539ad81ba2a2380619f09e4b8fda
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-17 11:48:38 -0600

    not sure if this is how i want to do it

--- Log.cs
+++ Log.cs
@@ -13,4 +13,27 @@ public class Log : IDisposable
     /// <param name="msg">The object to write.</param>
     /// <returns>A <see cref="Task"/> which can be <see langword="await"/>ed.</returns>
     public delegate Task Delegate(object? msg);
+    private IEnumerable<Delegate> _delegates;
+    private IEnumerable<Action> _disposals;
+    private bool _disposedValue;
+
+    public Log(params DelegatePair[] pairs)
+    {
+
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+                foreach (Action dispose in _disposals)
+                    dispose();
+            _disposedValue = true;
+        }
+    }
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
