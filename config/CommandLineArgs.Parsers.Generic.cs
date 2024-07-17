dd9f8f5bbe917e05be5cc34eac3564edd2abf700
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-17 11:37:33 -0600

    CommandLineArgs.Parsers testing work from a bit ago

--- config/CommandLineArgs.Parsers.Generic.cs
+++ config/CommandLineArgs.Parsers.Generic.cs
@@ -8,18 +8,34 @@ public static partial class CommandLineArgs
     /// </summary>
     public static partial class Parsers
     {
-        private static IEnumerable<T?> _parsables<T>(IEnumerable<string>? values, IFormatProvider? formatProvider)
-            where T : IParsable<T>
+        private static IEnumerable<T?> _structs<T>(IEnumerable<string>? values, IFormatProvider? formatProvider, bool includeNull = true)
+            where T : struct, IParsable<T>
         {
             if (values is null)
                 yield break;
             foreach (string s in values)
-                if (T.TryParse(s, formatProvider ?? CultureInfo.InvariantCulture, out T? result))
-                    yield return result;
+            {
+                if(T.TryParse(s, formatProvider ?? CultureInfo.InvariantCulture, out T result) || includeNull)
+                yield return result;
+            }
         }
-        public static Parser<IEnumerable<T?>> Parsables<T>(IFormatProvider? formatProvider = null)
-            where T : IParsable<T>
-            => (values, _) => _parsables<T>(values, formatProvider);
+        private static IEnumerable<T?> _classes<T>(IEnumerable<string>? values, IFormatProvider? formatProvider)
+            where T : class, IParsable<T>
+        {
+            if (values is null)
+                yield break;
+            foreach (string s in values)
+            {
+                T.TryParse(s, formatProvider ?? CultureInfo.InvariantCulture, out T? result);
+                yield return result;
+            }
+        }
+        public static Parser<IEnumerable<T?>> Structs<T>(IFormatProvider? formatProvider = null)
+            where T : struct, IParsable<T>
+            => (values, _) => _structs<T>(values, formatProvider);
+        public static Parser<IEnumerable<T?>> Classes<T>(IFormatProvider? formatProvider = null)
+            where T : class, IParsable<T>
+            => (values, _) => _classes<T>(values, formatProvider);
         /// <summary>
         /// Parses a reference type which implements <see cref="IParsable{TSelf}"/> from the first
         /// parsable string in the given values.
@@ -34,9 +50,9 @@ public static partial class CommandLineArgs
         /// An instance of <typeparamref name="T"/>, if any of the <c>values</c> can be successfully
         /// parsed, or <see langword="null"/> otherwise.
         /// </returns>
-        public static Parser<T?> Parsable<T>(IFormatProvider? formatProvider = null)
-            where T : IParsable<T>
-            => (values, _) => _parsables<T>(values, formatProvider).FirstOrDefault();
+        public static Parser<T?> Struct<T>(IFormatProvider? formatProvider = null)
+            where T : struct, IParsable<T>
+            => (values, _) => _structs<T>(values, formatProvider, false).FirstOrDefault();
         /// <summary>
         /// Parses a value type which implements <see cref="IParsable{TSelf}"/> from the first
         /// parsable string in the given values.
@@ -51,8 +67,8 @@ public static partial class CommandLineArgs
         /// An instance of <typeparamref name="T"/>, if any of the <c>values</c> can be successfully
         /// parsed, or <see langword="null"/> otherwise.
         /// </returns>
-        public static Parser<T?> Struct<T>(IFormatProvider? formatProvider = null)
-            where T : struct, IParsable<T>
+        public static Parser<T?> Class<T>(IFormatProvider? formatProvider = null)
+            where T : class, IParsable<T>
             => (values, _) =>
             {
                 if (values is null)
--- test/Tests_CommandLineArgs_Parsers_Generic.cs
+++ test/Tests_CommandLineArgs_Parsers_Generic.cs
@@ -7,6 +7,7 @@ public class Tests_CommandLineArgs_Parsers_Generic
     [TestMethod]
     public void Test_Parsables()
     {
-        CommandLineArgs.Parser<IEnumerable<double?>> parser = CommandLineArgs.Parsers.Parsables<double?>();
+        CommandLineArgs.Parser<IEnumerable<double?>> parser = CommandLineArgs.Parsers.Structs<double>();
+        Assert.AreEqual(null, parser(new string[] { "invalid" }, false)?.First());
     }
 }
