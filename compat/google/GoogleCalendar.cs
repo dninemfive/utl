0589ad0d1a652fef70767a80906e0704a174ccac
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-20 13:18:18 -0600

    instance GoogleUtils and break up the code into separate files

---
+++ compat/google/GoogleCalendar.cs
@@ -0,0 +1,45 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+namespace d9.utl.compat.google;
+public partial class GoogleCalendar
+{
+    public string Id { get; private set; }
+    public CalendarService Service { get; private set; }
+    public GoogleCalendar(string id, CalendarService service)
+    {
+        Id = id;
+        Service = service;
+    }
+    public GoogleCalendar(string id, GoogleAuth auth) : this(id, new(new BaseClientService.Initializer()
+    {
+        HttpClientInitializer = auth.Credential(CalendarService.Scope.Calendar),
+        ApplicationName = auth.AppName
+    }));
+    /// <summary>
+    /// Adds an event to the associated calendar.
+    /// </summary>
+    /// <param name="newEvent">The event to add to the calendar.</param>
+    /// <returns>The created event.</returns>
+    public Event Add(Event newEvent)
+    {
+        EventsResource.InsertRequest request = new(Service, newEvent, Id);
+        Event result = request.Execute();
+        Utils.DebugLog($"Event {result.Id} \"{result.Summary}\" created.");
+        return result;
+    }
+    /// <summary>
+    /// Updates a specified event on a specified calendar.
+    /// </summary>
+    /// <param name="eventId">The ID of the event to update.</param>
+    /// <param name="newEvent">The event with which to replace the specified event.</param>
+    /// <returns>The updated event.</returns>
+    public Event Update(string eventId, Event newEvent)
+    {
+        EventsResource.UpdateRequest request = new(Service, newEvent, Id, eventId);
+        Event result = request.Execute();
+        Utils.DebugLog($"Event {result.Id} \"{result.Summary}\" updated.");
+        return result;
+    }
+}
---
+++ compat/google/GoogleUtils.Drive.cs
@@ -0,0 +1,53 @@
+﻿using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.utl.compat.google;
+public partial class GoogleAuth
+{
+    /// <summary>
+    /// Gets a 
+    /// <see href="https://developers.google.com/resources/api-libraries/documentation/drive/v3/csharp/latest/classGoogle_1_1Apis_1_1Drive_1_1v3_1_1DriveService.html">
+    ///     drive service
+    /// </see>
+    /// using a <see cref="Credential">Credential</see> scoped to allow all Drive operations.
+    /// </summary>
+    public DriveService DriveService
+        => new(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = Credential(DriveService.Scope.Drive),
+            ApplicationName = Config!.AppName
+        });
+    /// <summary>
+    /// Attempts to download a file from a Drive URL to the <paramref name="filePath">specified path</paramref>
+    /// and prints whether or not it was successful, as well as the response code.
+    /// </summary>
+    /// <remarks>The file must be shared, through the Sheets UI, with the <see cref="GoogleAuthConfig.Email">email associated with the service account</see>.</remarks>
+    /// <param name="fileId">The Drive ID of the file to download.</param>
+    /// <param name="filePath">The path to the file when downloaded.</param>
+    /// <param name="mimeType">The type of the file to download. Should be a valid 
+    /// <see href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types">MIME type</see>.</param>
+    /// <returns>The path to the downloaded file, if successfully downloaded, or <see langword="null"/> otherwise.</returns>
+    public string? Download(string fileId, string filePath, string mimeType)
+    {
+        FilesResource.ExportRequest request = new(DriveService, fileId, mimeType);
+        filePath = filePath.AbsoluteOrInBaseFolder();
+        using FileStream fs = new(filePath, FileMode.Create);
+        IDownloadProgress progress = request.DownloadWithStatus(fs);
+        try
+        {
+            progress.ThrowOnFailure();
+            return filePath;
+        }
+        catch (Exception e)
+        {
+            Utils.DebugLog($"Error when downloading file from Drive!\n\t{e.Message}");
+            return null;
+        }
+    }
+}
