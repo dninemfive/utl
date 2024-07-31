56879143b85af1a611786a09f521f4d1f63de47f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-07-31 13:01:33 -0600

    move .Sieve() to CollectionUtils, rename Utils -> LogExtensions, remove static stuff therefrom

--- config/CommandLineArgs.Parsers.FileSystem.cs
+++ config/CommandLineArgs.Parsers.FileSystem.cs
@@ -38,7 +38,7 @@ public static partial class CommandLineArgs
             }
             catch (Exception e)
             {
-                Utils.DebugLog(e);
+                LogExtensions.DebugLog(e);
                 return null;
             }
             return path;
@@ -66,7 +66,7 @@ public static partial class CommandLineArgs
             }
             catch (Exception e)
             {
-                Utils.DebugLog(e);
+                LogExtensions.DebugLog(e);
                 return null;
             }
             if (!File.Exists(path))
