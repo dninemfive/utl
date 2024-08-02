4d965961eafd7f129c76dc9ed4b9e9c5cdfc8d9b
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-02 13:03:25 -0600

    more general GoogleServiceWrapper code

--- compat/google/GoogleCalendar.cs
+++ compat/google/GoogleCalendar.cs
@@ -1,19 +1,58 @@
-﻿using Google.Apis.Calendar.v3.Data;
-using Google.Apis.Calendar.v3;
-using Google.Apis.Services;
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
 
 namespace d9.utl.compat.google;
+/// <summary>
+/// Wrapper for a specific Google Calendar with a particular ID with methods to add and update
+/// events on the calendar.
+/// </summary>
 public partial class GoogleCalendar
     : GoogleServiceWrapper<CalendarService>
 {
+    /// <inheritdoc cref="GoogleServiceWrapper{T}.GoogleServiceWrapper(T, GoogleServiceContext)"/>
+#pragma warning disable CS8618 // Id must be non-null: guaranteed to be set by reflection unless an implementer does something stupid
+    protected GoogleCalendar(CalendarService service, GoogleServiceContext context)
+        : base(service, context) { }
+#pragma warning restore CS8618
+    /// <summary>
+    /// The ID of the Google calendar this particular instance will modify.
+    /// </summary>
     public string Id { get; private set; }
-    public GoogleCalendar(string id, CalendarService service) 
-        : base(service)
+    /// <summary>
+    /// Tries to create a Google Calendar wrapper in the specified <paramref name="context"/>,
+    /// modifying the calendar with the specified <paramref name="calendarId"/>.
+    /// </summary>
+    /// <param name="context">The context in which this Google Calendar will be created.</param>
+    /// <param name="calendarId">The ID of the calendar this wrapper will edit.</param>
+    /// <returns>A new Google Calendar wrapper, if successful, or <see langword="null"/> otherwise.</returns>
+    public static GoogleCalendar? TryCreateFrom(GoogleServiceContext context, string calendarId)
     {
-        Id = id;
+        GoogleCalendar? calendar = TryCreate<GoogleCalendar>(context, CalendarService.Scope.Calendar);
+        if (calendar is GoogleCalendar result)
+        {
+            result.Id = calendarId;
+            return result;
+        }
+        return null;
     }
-    public GoogleCalendar(string id, GoogleServiceContext auth) 
-        : this(id, new CalendarService(auth.InitializerFor(CalendarService.Scope.Calendar))) { }
+    /// <summary>
+    /// Creates a Google Calendar wrapper in the specified <paramref name="context"/>, modifying the
+    /// calendar with the specified <paramref name="calendarId"/>, and <b>throwing an Exception</b>
+    /// if unsuccessful.
+    /// </summary>
+    /// <param name="context">The context in which this Google Calendar will be created.</param>
+    /// <param name="calendarId">The ID of the calendar this wrapper will edit.</param>
+    /// <returns>A new Google Calendar wrapper.</returns>
+    /// <exception cref="Exception">
+    /// Thrown if creation of the wrapper was unsuccesful for any reason.
+    /// </exception>
+    public static GoogleCalendar CreateFrom(GoogleServiceContext context, string calendarId)
+    {
+        GoogleCalendar? calendar = TryCreateFrom(context, calendarId);
+        if (calendar is GoogleCalendar result)
+            return result;
+        throw new Exception($"Could not initialize {typeof(GoogleCalendar).Name} with context {context} and ID {calendarId}!");
+    }
     /// <summary>
     /// Adds an event to the associated calendar.
     /// </summary>
--- compat/google/GoogleDrive.cs
+++ compat/google/GoogleDrive.cs
@@ -2,21 +2,50 @@
 using Google.Apis.Drive.v3;
 
 namespace d9.utl.compat.google;
+/// <summary>
+/// Wrapper for a Google Drive service with a helper method to download files therefrom.
+/// </summary>
 public class GoogleDrive
     : GoogleServiceWrapper<DriveService>
 {
-    public GoogleDrive(GoogleServiceContext context) 
-        : this(new DriveService(context.InitializerFor(DriveService.Scope.Drive)), context.Log) { }
+    /// <inheritdoc cref="GoogleServiceWrapper{T}.GoogleServiceWrapper(T, GoogleServiceContext)"/>
+    protected GoogleDrive(DriveService service, GoogleServiceContext context)
+        : base(service, context) { }
     /// <summary>
-    /// Attempts to download a file from a Drive URL to the <paramref name="filePath">specified path</paramref>
-    /// and prints whether or not it was successful, as well as the response code.
+    /// Tries to create a Google Drive wrapper in the specified <paramref name="context"/>.
     /// </summary>
-    /// <remarks>The file must be shared, through the Sheets UI, with the <see cref="GoogleAuthConfig.Email">email associated with the service account</see>.</remarks>
+    /// <param name="context">The context in which this Google Drive wrapper will be created.</param>
+    /// <returns>A new Google Drive wrapper, if successful, or <see langword="null"/> otherwise.</returns>
+    public static GoogleDrive? TryCreateFrom(GoogleServiceContext context)
+        => TryCreate<GoogleDrive>(context, DriveService.Scope.Drive);
+    /// <summary>
+    /// Creates a Google Drive wrapper in the specified <paramref name="context"/>, <b>throwing an
+    /// Exception</b> if unsuccessful.
+    /// </summary>
+    /// <param name="context">The context in which this Google Drive wrapper will be created.</param>
+    /// <returns>A new Google Drive wrapper.</returns>
+    /// <exception cref="Exception">
+    /// Thrown if creation of the wrapper was unsuccesful for any reason.
+    /// </exception>
+    public static GoogleDrive CreateFrom(GoogleServiceContext context)
+        => Create<GoogleDrive>(context, DriveService.Scope.Drive);
+    /// <summary>
+    /// Attempts to download a file from a Drive URL to the <paramref name="filePath">specified
+    /// path</paramref> and prints whether or not it was successful, as well as the response code.
+    /// </summary>
+    /// <remarks>
+    /// The file must be shared, through the Sheets UI, with the <see
+    /// cref="GoogleAuthConfig.Email">email associated with the service account</see>.
+    /// </remarks>
     /// <param name="fileId">The Drive ID of the file to download.</param>
     /// <param name="filePath">The path to the file when downloaded.</param>
-    /// <param name="mimeType">The type of the file to download. Should be a valid 
-    /// <see href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types">MIME type</see>.</param>
-    /// <returns>The path to the downloaded file, if successfully downloaded, or <see langword="null"/> otherwise.</returns>
+    /// <param name="mimeType">
+    /// The type of the file to download. Should be a valid <see
+    /// href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types">MIME type</see>.
+    /// </param>
+    /// <returns>
+    /// The path to the downloaded file, if successfully downloaded, or <see langword="null"/> otherwise.
+    /// </returns>
     public string? Download(string fileId, string filePath, string mimeType)
     {
         FilesResource.ExportRequest request = new(Service, fileId, mimeType);
