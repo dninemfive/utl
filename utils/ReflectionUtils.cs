b18b38db835cb7404dea61a3379b8af492a91837
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-18 21:13:46 -0600

    remove references to AppDomain.CurrentDomain in ReflectionUtils, as this causes issues with other assemblies; remove several unnecessary extension methods therein

--- utils/ReflectionUtils.cs
+++ utils/ReflectionUtils.cs
@@ -5,17 +5,6 @@ namespace d9.utl;
 /// </summary>
 public static class ReflectionUtils
 {
-    #region Assembly
-    /// <summary>
-    /// Filters all loaded assemblies with the specified <c>selector</c>, if any.
-    /// </summary>
-    /// <param name="selector">The function to use to filter the assemblies. If <see langword="null"/>, all loaded assemblies will be included.</param>
-    /// <returns>All loaded assemblies, if the <c>selector</c> is <see langword="null"/>, or all loaded assemblies where the <c>selector</c> returns
-    /// <see langword="true"/>, if it is not <see langword="null"/>.</returns>
-    public static IEnumerable<Assembly> AllLoadedAssemblies(Func<Assembly, bool>? selector = null)
-    {
-        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) if (selector is null || selector(assembly)) yield return assembly;
-    }
     /// <summary>
     /// Tells whether the specified assembly has the specified attribute.
     /// </summary>
@@ -23,9 +12,7 @@ public static class ReflectionUtils
     /// <param name="assembly">The assembly to look for the attribute on.</param>
     /// <returns><see langword="true"/> if <c>assembly</c> has an <see cref="Attribute"/> of type <c>T</c>, or <see langword="false"/> otherwise.</returns>
     public static bool HasCustomAttribute<T>(this Assembly assembly) where T : Attribute
-        => assembly.GetCustomAttribute(typeof(T)) is not null;
-    #endregion Assembly
-    #region MemberInfo
+        => assembly.GetCustomAttribute<T>() is not null;
     /// <summary>
     /// Returns the members of the specified type with the specified attribute.
     /// </summary>
@@ -55,29 +42,23 @@ public static class ReflectionUtils
         if (declaringType?.Namespace is not null) result = $"{declaringType.Namespace}::{result}";
         return result;
     }
-    #endregion MemberInfo
     #region Type
     /// <summary>
     /// Filters the types in all loaded assemblies based on a given function.
     /// </summary>
-    /// <param name="selector">The function to use to filter the assemblies. If it returns <see langword="true"/> for a given type, that type is included;
-    /// otherwise, it is not included.</param>
+    /// <param name="domain">The AppDomain whose assemblies to search.</param>
     /// <returns>An <see cref="IEnumerable{T}">IEnumerable</see>&lt;<see cref="Type"/>&gt; where every element <c>t</c> is present if and only if
     /// <c>selector(t)</c> returns <see langword="true"/>.</returns>
-    public static IEnumerable<Type> AllLoadedTypes(Func<Type, bool>? selector = null)
-    {
-        foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            foreach (Type type in assembly.TypesWhere(x => selector is null || selector(x))) yield return type;
-        }
-    }
+    public static IEnumerable<Type> AllLoadedTypes(this AppDomain domain)
+        => domain.GetAssemblies().SelectMany(x => x.GetTypes());
     /// <summary>
     /// Selects the types in loaded assemblies which have the specified attribute.
     /// </summary>
     /// <typeparam name="T">The <see cref="Type"/> of the attribute to select. Must inherit from <see cref="Attribute"/>.</typeparam>
+    /// <param name="domain">The AppDomain whose assemblies to search.</param>
     /// <returns>An <see cref="IEnumerable{T}">IEnumerable</see>&lt;<see cref="Type"/>&gt; where every element has the specified attribute.</returns>
-    public static IEnumerable<Type> AllLoadedTypesWithAttribute<T>() where T : Attribute
-        => AllLoadedTypes(x => x.HasCustomAttribute<T>());
+    public static IEnumerable<Type> AllLoadedTypesWithAttribute<T>(this AppDomain domain) where T : Attribute
+        => domain.AllLoadedTypes().Where(x => x.HasCustomAttribute<T>());
     /// <summary>
     /// Tells whether the specified type has the specified attribute.
     /// </summary>
@@ -86,45 +67,5 @@ public static class ReflectionUtils
     /// <returns><see langword="true"/> if <c>type</c> has an <see cref="Attribute"/> of type <c>T</c>, or <see langword="false"/> otherwise.</returns>
     public static bool HasCustomAttribute<T>(this Type type) where T : Attribute
         => type.GetCustomAttribute<T>() is not null;
-    /// <summary>
-    /// Iterates over all loaded assemblies, and from the ones with the specified attribute, selects the types with the same attribute.
-    /// </summary>
-    /// <typeparam name="T">The <see cref="Type"/> of the attribute to select. Must inherit from <see cref="Attribute"/>.</typeparam>
-    /// <returns></returns>
-    public static IEnumerable<Type> TypesInAssembliesWithAttribute<T>() where T : Attribute
-        => TypesInAssembliesWhere(x => x.HasCustomAttribute<T>(), x => x.HasCustomAttribute<T>());
-    #endregion Type
-    #region Assembly x Type
-    /// <summary>
-    /// Iterates over all loaded assembly, and from the ones which the <c>assemblySelector</c> returns <see langword="true"/> on,
-    /// returns the types the <c>typeSelector</c>, if any, returns <see langword="true"/> on.
-    /// </summary>
-    /// <param name="assemblySelector">A function which decides whether to include a given assembly.</param>
-    /// <param name="typeSelector">A function which decides whether to include a given type.</param>
-    /// <returns>An <see cref="IEnumerable{T}">IEnumerable</see>&lt;<see cref="Type"/>&gt; where every element <c>t</c> causes 
-    /// <c>typeSelector</c> to return <see langword="true"/>, and whose declaring assembly causes <c>assemblySelector</c> to return <see langword="true"/>.</returns>
-    public static IEnumerable<Type> TypesInAssembliesWhere(Func<Assembly, bool> assemblySelector, Func<Type, bool>? typeSelector = null)
-    {
-        foreach (Assembly assembly in AllLoadedAssemblies(assemblySelector))
-        {
-            if (typeSelector is null)
-            {
-                foreach (Type type in assembly.GetTypes()) yield return type;
-            }
-            else
-            {
-                foreach (Type type in assembly.TypesWhere(x => typeSelector(x))) yield return type;
-            }
-        }
-    }
-    /// <summary>
-    /// Filters the types in an assembly based on a given function.
-    /// </summary>
-    /// <param name="assembly">The assembly whose types to filter.</param>
-    /// <param name="selector">The function to use to filter the assembly. If it returns <see langword="true"/> for a given type, that type is included;
-    /// otherwise, it is not included.</param>
-    /// <returns>An <see cref="IEnumerable{T}">IEnumerable</see>&lt;<see cref="Type"/>&gt; where every element <c>t</c> is present if and only if
-    /// <c>selector(t)</c> returns <see langword="true"/>.</returns>
-    public static IEnumerable<Type> TypesWhere(this Assembly assembly, Func<Type, bool> selector) => assembly.GetTypes().Where(x => selector(x));
-    #endregion Assembly x Type        
+    #endregion Type    
 }
