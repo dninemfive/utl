533bf78b034d039f583907b02c6c6d3c7bcf2e8c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-05-24 13:46:10 -0600

    implement DefaultDictionary (#27)

--- types/Dictionaries/DefaultDictionary.cs
+++ types/Dictionaries/DefaultDictionary.cs
@@ -5,7 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace d9.utl.types;
-public class DefaultDictionary<K, V> : IEnumerable<KeyValuePair<K, V>>, IDictionary<K, V>, IReadOnlyDictionary<K, V>
+public class DefaultDictionary<K, V>
+    : BaseDictionary<K, V>
     where K : notnull
 {
+    private readonly Func<K, V> _defaultFrom;
+    public DefaultDictionary(Func<K, V> defaultFunction) 
+        : base()
+        => _defaultFrom = defaultFunction;
+    public DefaultDictionary(IEnumerable<KeyValuePair<K, V>> values, Func<K, V> defaultFunction)
+        : base(values)
+        => _defaultFrom = defaultFunction;
+    public DefaultDictionary(IEnumerable<(K key, V value)> values, Func<K, V> defaultFunction)
+        : base(values)
+        => _defaultFrom = defaultFunction;
+    protected override V GetDefaultValue(K key)
+        => _defaultFrom(key);
 }
