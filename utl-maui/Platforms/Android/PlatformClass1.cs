7d0657128e433c7f1ed7f7fa4154d36e8d659da8
dninemfive <11084010+dninemfive@users.noreply.github.com>
2025-05-29 12:01:15 -0600

    change root folder structure for increased modularity

--- utl-maui/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace utl_maui
-{
-    // All the code in this file is only included on Android.
-    public class PlatformClass1
-    {
-    }
-}
--- utl-maui/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace utl_maui
-{
-    // All the code in this file is only included on iOS.
-    public class PlatformClass1
-    {
-    }
-}
